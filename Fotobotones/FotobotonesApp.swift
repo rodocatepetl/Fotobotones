32bdd5857006a7c323fdc1b6476089c55740c2b6
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-02-08 16:20:30 -0600

    Initial Commit

---
+++ Fotobotones/FotobotonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotobotonesApp.swift
+//  Fotobotones
+//
+//  Created by Rodolfo Castillo on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FotobotonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
